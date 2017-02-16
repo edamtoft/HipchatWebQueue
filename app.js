2b1ac5e9979bc3dee8d8b068b1f9e0232d2c7383
Eric Damtoft <edamtoft@gmail.com>
2017-02-15 23:56:12 -0500

    Ignore config.json, add some logging

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ Amqp.connect(process.env.AMQP_CONN, (err,conn) => {
   conn.createChannel((err, channel) => {
     if (err) throw err;
 
+    console.log("Connected to AMQU channel");
+
     const app = new Express();
     
     app.use(BodyParser.json());
-    app.post("/message", (req,res) => channel.publish("hipchat_messages", req.body.webhook_id, new Buffer(JSON.stringify(req.body))));
+    app.post("/message", (req,res) => {
+      channel.publish("hipchat_messages", req.body.webhook_id, new Buffer(JSON.stringify(req.body)));
+      console.info(`Published message on channel ${req.body.webhook_id}`);
+    });
     
     const server = Http.createServer(app);
-    server.listen()
+    server.listen(3000, () => console.info("Server Running"));
   });
 });
